af5c94cf0a2949fb2b82643b51bce1cdc995ed1c
BuiHuyTam <ht2812002@gmail.com>
2024-04-20 21:37:14 +0700

    feat: finish set up signup page

--- app/src/main/java/com/example/notesapplication/CreateAccountActivity.java
+++ app/src/main/java/com/example/notesapplication/CreateAccountActivity.java
@@ -1,12 +1,21 @@
 package com.example.notesapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class CreateAccountActivity extends AppCompatActivity {
     EditText emailEditText, passwordEditText, confirmPasswordEditText;
@@ -25,5 +34,63 @@ public class CreateAccountActivity extends AppCompatActivity {
         progressBar = findViewById(R.id.progress_bar);
         loginBtnTextView = findViewById(R.id.login_text_view_btn);
 
+        createAccountBtn.setOnClickListener(v -> createAccount());
+        loginBtnTextView.setOnClickListener(v -> finish());
+    }
+    void createAccount(){
+        String email = emailEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+        String confirm_password = confirmPasswordEditText.getText().toString();
+        boolean isValidate = validateData(email, password, confirm_password);
+        if(!isValidate){
+            return;
+        }
+        createAccountInFirebase(email,password);
+    }
+    void createAccountInFirebase(String email, String password){
+        changeInProgess(true);
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(CreateAccountActivity.this,
+                new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            changeInProgess(false);
+                            Toast.makeText(CreateAccountActivity.this,
+                                    "Successfully create account, Check email to verify", Toast.LENGTH_LONG).show();
+                            firebaseAuth.getCurrentUser().sendEmailVerification();
+                            firebaseAuth.signOut();
+                            finish();
+                        }else{
+                            Toast.makeText(CreateAccountActivity.this,
+                                    task.getException().getLocalizedMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+
+    }
+    void changeInProgess(boolean inProgress){
+        if(inProgress){
+            progressBar.setVisibility(View.VISIBLE);
+            createAccountBtn.setVisibility(View.GONE);
+        }else{
+            progressBar.setVisibility(View.GONE);
+            createAccountBtn.setVisibility(View.VISIBLE);
+        }
+    }
+    boolean validateData(String email, String password, String confirm_password){
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            emailEditText.setError("Email is invalid");
+            return false;
+        }
+        if(password.length() < 6){
+            passwordEditText.setError("Password length is invalid");
+            return false;
+        }
+        if(!password.equals(confirm_password)){
+            confirmPasswordEditText.setError("Password not matched");
+            return false;
+        }
+        return true;
     }
 }
