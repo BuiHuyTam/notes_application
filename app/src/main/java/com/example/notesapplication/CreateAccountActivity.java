a19781c476a626428b25bd0c7d6e278f9f0de6f7
BuiHuyTam <ht2812002@gmail.com>
2024-04-20 12:02:57 +0700

    feat: set up firebase authentication

--- app/src/main/java/com/example/notesapplication/CreateAccountActivity.java
+++ app/src/main/java/com/example/notesapplication/CreateAccountActivity.java
@@ -3,12 +3,27 @@ package com.example.notesapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class CreateAccountActivity extends AppCompatActivity {
+    EditText emailEditText, passwordEditText, confirmPasswordEditText;
+    Button createAccountBtn;
+    ProgressBar progressBar;
+    TextView loginBtnTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+        emailEditText = findViewById(R.id.email_edit_text);
+        passwordEditText = findViewById(R.id.password_edit_text);
+        confirmPasswordEditText = findViewById(R.id.confirm_password_edit_text);
+        createAccountBtn = findViewById(R.id.create_account_btn);
+        progressBar = findViewById(R.id.progress_bar);
+        loginBtnTextView = findViewById(R.id.login_text_view_btn);
+
     }
 }
